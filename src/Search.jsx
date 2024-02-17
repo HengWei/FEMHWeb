84fa3e2b68ddca0a6fcb648769f39ba752be5992
Henry <swatduck@gmail.com>
2024-02-17 18:17:42 +0800

    新增_搜尋頁面

---
+++ src/Search.jsx
@@ -0,0 +1,55 @@
+﻿import queryString from "query-string";
+import medicationData from './Medication.json';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+function Search() {
+    let searchItem = [];
+
+    //搜尋資料組合
+    medicationData.data.map((parent, i) =>
+        parent.child.map((child, i) =>
+            searchItem.push({
+                title: child.title,
+                url: parent.url
+            })
+        )
+    )
+
+    //參數取得
+    const parsed = queryString.parse(window.location.search);
+
+    //過濾資料
+    let result = searchItem.filter(x => x.title.includes(parsed.q));
+
+    //顯示內容
+    let show;
+
+    if (result.length === 0) {
+        show = <h3>查無資料</h3>
+    }
+    else {
+        show = <ListGroup>
+            {result.map((item, i) =>
+                <ListGroup.Item key={i} action href={item.url} >
+                    {item.title}
+                </ListGroup.Item>)}
+        </ListGroup>
+    }
+
+    return (
+        <>
+            <Container style={{ minHeight: 800, paddingTop: 20 }} >
+                <Row>
+                    <h1>搜尋結果</h1>
+                </Row>
+                <Row>
+                    {show}
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default Search;
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import Gout from './disease/Gout'
 
 import Symptom from './Symptom'
 import Lab from './Lab'
+import Search from './Search'
 
 
 import Medication from './MedicationIdx'
@@ -19,6 +20,13 @@ import Nsaid from './Medication/Nsaid'
 import Steroid from './Medication/Steroid'
 import Biologicalagents from './Medication/Biologicalagents'
 import Immunomodulator from './Medication/Immunomodulator'
+import Moleculetargetdrug from './Medication/Moleculetargetdrug'
+import Goutmedicine from './Medication/Goutmedicine'
+import MOsteoporosis from './Medication/M-Osteoporosis'
+import Antiallergy from './Medication/Antiallergy'
+import Musclerelaxant from './Medication/Musclerelaxant'
+
+
 
 
 import { BrowserRouter, Route, Routes } from "react-router-dom"
@@ -40,7 +48,13 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                 <Route path="/Steroid" element={<Steroid />} />
                 <Route path="/Immunomodulator" element={<Immunomodulator />} />
                 <Route path="/Biologicalagents" element={<Biologicalagents />} />
+                <Route path="/Moleculetargetdrug" element={<Moleculetargetdrug />} />
+                <Route path="/Goutmedicine" element={<Goutmedicine />} />
+                <Route path="/M-Osteoporosis" element={<MOsteoporosis />} />
+                <Route path="/Antiallergy" element={<Antiallergy />} />
+                <Route path="/Musclerelaxant" element={<Musclerelaxant />} />
                 <Route path="/Gout" element={<Gout />} />
+                <Route path="/Search" element={<Search />} />
 
             </Routes>
         </BrowserRouter>
